f27a73e9dbf9d9ac3dcc86dd74d2c1dbe7bd0e19
Tiffany Monique Matamoros <70666921+TiffMonique@users.noreply.github.com>
2021-12-17 12:08:38 -0600

    a ver

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+/*$("#LoadWebApi").click(function () {
+
+    $.ajax({
+        type: "GET",
+        url: "https://digimon-api.herokuapp.com/api/digimon",
+        dataType: "json",
+
+        success: function (data) {
+            $.each(data, function (i, item) {
+                var row = "<tr>" +
+                    "<td>" + item.name + "</td>" +
+                    "<td>" + item.level + "</td>" +
+                    "</tr>";
+
+                $("#tabla>tbody").append(row);
+
+            });
+        },
+
+    });
+
+
+});*/
+
+// $(document).click(function () {
+$(document).ready(function () {
+
+    $.ajax({
+        type: "GET",
+        url: "https://digimon-api.herokuapp.com/api/digimon",
+
+        dataType: "json",
+
+        success: function (data) {
+            $.each(data, function (i, item) {
+
+                var row = "<tr>" +
+                    "<td>" + item.name + "</td>" +
+                    "<td>" + item.level + "</td>" +
+                    "</tr>";
+
+                $("#tabla>tbody").append(row);
+
+            });
+        },
+
+    });
+
+
+});
+
